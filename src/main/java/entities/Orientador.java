9af63ef1baa8ec4aacaa4da87ad18fdf4bdfa5f3
Wanielton F. Silva <wanieltonferreira@gmail.com>
2023-12-23 01:10:40 -0300

    Entidade Orientador

--- src/main/java/entities/Orientador.java
+++ src/main/java/entities/Orientador.java
@@ -1,1 +1,44 @@
+package entities;
+
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Orientador {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+
+    @OneToMany(mappedBy = "orientador")
+    private List<Aluno> alunos;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Aluno> getAlunos() {
+        return alunos;
+    }
+
+    public void setAlunos(List<Aluno> alunos) {
+        this.alunos = alunos;
+    }
+}
 
