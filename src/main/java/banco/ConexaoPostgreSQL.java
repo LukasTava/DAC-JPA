a00f92116b652836b82bb0d5733741fc673e1b83
Wanielton F. Silva <wanieltonferreira@gmail.com>
2023-12-22 23:40:40 -0300

    Conexão e servelet

--- src/main/java/banco/ConexaoPostgreSQL.java
+++ src/main/java/banco/ConexaoPostgreSQL.java
@@ -4,6 +4,22 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+public class ConexaoPostgreSQL {
+    public static void main(String[] args) {
+        String jdbcURL = "jdbc:postgresql://localhost:5432/postgres";
+        String username = "postgres";
+        String password = "123";
 
+        try {
+            Connection connection = DriverManager.getConnection(jdbcURL, username, password);
+            if (connection != null) {
+                System.out.println("Conexão com o PostgreSQL estabelecida com sucesso.");
+                connection.close();
+            }
+        } catch (SQLException e) {
+            System.err.println("Erro ao conectar ao PostgreSQL: " + e.getMessage());
+        }
+    }
+}
 
 
