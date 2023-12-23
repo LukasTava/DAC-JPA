ce3e920ddcb9780d0f3d9f2455d57e81fa54c776
Wanielton F. Silva <wanieltonferreira@gmail.com>
2023-12-22 23:49:16 -0300

    Aluno

---
+++ src/main/java/entities/Aluno.java
@@ -0,0 +1,49 @@
+package entities;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class Aluno {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String matricula;
+    private String nome;
+    @ManyToOne
+    private Empresa empresa;
+
+    @OneToMany(mappedBy = "aluno")
+    private List<Estagio> estagios;
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Empresa getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(Empresa empresa) {
+        this.empresa = empresa;
+    }
+
+    public List<Estagio> getEstagios() {
+        return estagios;
+    }
+
+    public void setEstagios(List<Estagio> estagios) {
+        this.estagios = estagios;
+    }
+}
